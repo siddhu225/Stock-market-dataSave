132c39c798a5c41aebb6154a54c99b5b5878b5fd
sai siddardha <34027249+siddhu225@users.noreply.github.com>
2021-10-31 22:07:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,37 +1,65 @@
 const express = require('express');
 require('./db/mongoose');
 const cors = require('cors');
-const StockModel = require('./models/stock');
-
-
-let firstStock = {
-  'Nasdaq Traded': 'Y',
-  Symbol: 'A',
-  'Security Name': 'Agilent Technologies, Inc. Common Stock',
-  'Listing Exchange': 'N',
-  'Market Category': ' ',
-  ETF: 'N',
-  'Round Lot Size': '100.0',
-  'Test Issue': 'N',
-  'Financial Status': '',
-  'CQS Symbol': 'A',
-  'NASDAQ Symbol': 'A',
-  NextShares: 'N'
-};
-
-// This stock addition is to create DB and its collection.
-(async () => {
-  firstStock = new StockModel(firstStock)
-  await firstStock.save();
-})();
-
-
+const { ETFModel, StockModel } = require('./models/StockModel');
+const companies = require('./models/companies');
+const _ = require('lodash');
+let ObjectID = require('mongoose').mongo.ObjectId;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-require('./scripts/AddDataFromCSV');
+
+// Script for bulk insert data from stocks and etf folder
+
+require('./scripts/AddStockData');
+
+app.get('/stocks', async (req, res, next) => {
+  console.log(req.params, req.body, req.query);
+  try {
+    let reqObj = _.defaults(req.body, {
+      limit: 200,
+      page: 1,
+      type: 'stocks',
+      from: '',
+      to: ''
+    });
+    let companyIds = [];
+    if (_.get(req.body, 'companies', []).length > 0) {
+      let allComps = _.map(req.body.companies, (c) => c ? c.toUpperCase().trim() : null);
+      allComps = await companies.find({
+        name: _.compact(allComps),
+      });
+      companyIds = _.map(allComps, '_id');
+    }
+    console.log('companyIds', companyIds);
+    let model = reqObj.type === 'stocks' ? StockModel : ETFModel;
+    let queryObj = { };
+    if (reqObj.from) {
+      queryObj['availabledate'] = { 
+        $gte: new Date(reqObj.from) 
+      }
+    }
+    if (reqObj.to) {
+      queryObj['availabledate'] = { 
+        $lt: new Date(reqObj.to) 
+      }
+    }
+    if (companyIds) {
+      companyIds = _.filter(_.castArray(companyIds), a => ObjectID.isValid(a));
+      companyIds = _.map(companyIds, ObjectID);
+      queryObj['companyId'] = { $in: companyIds };
+    }
+    model.find(queryObj).limit(reqObj.limit).skip(reqObj.page).then((val) => {
+      res.status(200).json(val);
+    }).catch((err) => {
+      next(err);
+    })
+  } catch (e) {
+    console.log('error', e);
+  }
+});
 
 app.listen(8081, ()=> {
   console.log('app is listening at 8081');
